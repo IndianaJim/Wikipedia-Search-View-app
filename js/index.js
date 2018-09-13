71df9ead650a16943bda1b51f3b5c71b33a60b1c
indianajim <jimrunkel@gmail.com>
2018-09-13 16:11:56 -0400

    repaired overlapping results using array

--- js/index.js
+++ js/index.js
@@ -1,42 +1,57 @@
-"use strict";
+'use strict';
 
-$(document).ready(function () {
+$(document).ready(function() {
   //set focus in search input field
-  $("input:text:visible:first").focus();
+  $('input:text:visible:first').focus();
 
   //if search button clicked
-  $("#search").click(function () {
-    if ($("#searchTerm").val().length === 0) {
+  $('#search').click(function() {
+    if ($('#searchTerm').val().length === 0) {
       //shake search box if empty
-      $("#searchTerm").addClass("animated shake");
-      $('#searchTerm').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-        $("#searchTerm").removeClass("animated shake");
-      });
+      $('#searchTerm').addClass('animated shake');
+      $('#searchTerm').one(
+        'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+        function() {
+          $('#searchTerm').removeClass('animated shake');
+        }
+      );
     } else {
       doSearch();
     }
   });
   //Enter key already covered by 'keyup' test below
-  $("#searchTerm").keyup(function (k) {
+  $('#searchTerm').keyup(function(k) {
     doSearch();
   });
 });
 
 //search function
 function doSearch() {
-  var searchTerm = $("#searchTerm").val();
-  //console.log(searchTerm);
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&prop=text&limit=20&format=json&callback=?";
+  $('#output').empty(); // empty the div before fetching and adding new data
+  var items = [];
+  var searchTerm = $('#searchTerm').val();
+  var url =
+    'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +
+    searchTerm +
+    '&prop=text&limit=12&format=json&callback=?';
   $.ajax({
     url: url,
-    type: "GET",
+    type: 'GET',
     //async: false,
-    dataType: "json",
+    dataType: 'json',
     success: function success(data, status, jqXHR) {
-      //console.log(data);
-      for (var i = data[2].length; i >= 0; i--) {
-        $("#output").prepend("<div><div class='well well-sm animated flipInX'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2><p class='details'>" + data[2][i] + "</p></a></div></div>");
+      for (var i = data[2].length - 1; i >= 0; i--) {
+        items.unshift(
+          "<div><div class='well well-sm animated flipInX'><a href=" +
+            data[3][i] +
+            ' target="_blank"><h2>' +
+            data[1][i] +
+            "</h2><p class='details'>" +
+            data[2][i] +
+            '</p></a></div></div>'
+        );
       } //end for loop
+      $('#output').html(items);
     } //end success function
   }); //end ajax call
 } // end doSearch function
